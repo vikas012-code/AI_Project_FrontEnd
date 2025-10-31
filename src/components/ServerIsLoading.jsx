258d8f6233b6e7b778e962766562977af1533db0
vikas012-code <vikasthapa115@gmail.com>
2025-10-31 17:18:29 +0530

    added server loading

--- src/components/ServerIsLoading.jsx
+++ src/components/ServerIsLoading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ServerIsLoading() {
   return (
-    <div className='w-[100vw] h-[100vh] flex items-center justify-center absolute z-20 backdrop-blur-[2px] backdrop-grayscale-50'>
+    <div className='w-[100vw] h-[100vh] flex items-center justify-center fixed z-20 backdrop-blur-[2px] backdrop-grayscale-50'>
         <div className='w-[50%] h-[50%] max-sm:w-[70%] max-sm:h-[40%] bg-green-50 rounded-2xl p-10 flex flex-col items-center justify-center gap-4 relative'>
             <div className=' absolute top-6 left-6 right-6 max-sm:text-[10px]'>
                 <p><strong>Note</strong>-this is demo project so it is deployed in render's free tier because of that it's server sleeps after some time of if it is idle and takes 1-2 minuts to restart from sleep</p>
